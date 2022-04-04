51487467d053a39f4f7f25349fe7e6561e03e635
jalisa-b <jbroussard@utexas.edu>
2022-04-04 18:14:06 -0500

    added button scaling animation to disclaimer and menu fragment

--- app/src/main/java/com/example/homework3/DisclaimerFragment.kt
+++ app/src/main/java/com/example/homework3/DisclaimerFragment.kt
@@ -1,5 +1,9 @@
 package com.example.homework3
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.ObjectAnimator
+import android.animation.PropertyValuesHolder
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -12,6 +16,8 @@ import androidx.navigation.Navigation
 
 class DisclaimerFragment : Fragment() {
 
+    lateinit var nextButton: Button
+
 
     // using onCreateView to set up transition between fragments
     override fun onCreateView(
@@ -23,12 +29,36 @@ class DisclaimerFragment : Fragment() {
 
 
         //val disclaimerText = view.findViewById<TextView>(R.id.disclaimer)
-        val nextButton = view.findViewById<Button>(R.id.agree)
+        nextButton = view.findViewById<Button>(R.id.agree)
         nextButton.setOnClickListener{
-            Navigation.findNavController(view).navigate(R.id.action_disclaimerFragment_to_menuFragment)}
+
+            scaler()
+            Navigation.findNavController(view).navigate(R.id.action_disclaimerFragment_to_menuFragment)
+        }
 
             return view
 
+    }
+
+    private fun ObjectAnimator.disableViewDuringAnimation(view: View) {
+        addListener(object: AnimatorListenerAdapter(){
+            override fun onAnimationStart(animation: Animator?) {
+                view.isEnabled = false
+            }
+            override fun onAnimationEnd(animation: Animator?) {
+                view.isEnabled = true
+            }
+        })
+    }
+
+    private fun scaler() {
+        val scaleX = PropertyValuesHolder.ofFloat(View.SCALE_X,2f)
+        val scaleY = PropertyValuesHolder.ofFloat(View.SCALE_Y,2f)
+        val animator = ObjectAnimator.ofPropertyValuesHolder(nextButton,scaleX,scaleY)
+        animator.repeatCount = 1
+        animator.repeatMode = ObjectAnimator.REVERSE
+        animator.disableViewDuringAnimation(nextButton)
+        animator.start()
     }
 
 
--- app/src/main/java/com/example/homework3/MenuFragment.kt
+++ app/src/main/java/com/example/homework3/MenuFragment.kt
@@ -1,5 +1,9 @@
 package com.example.homework3
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.ObjectAnimator
+import android.animation.PropertyValuesHolder
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -12,6 +16,10 @@ import androidx.navigation.Navigation
 
 class MenuFragment : Fragment() {
 
+    lateinit var tipsButton: Button
+    lateinit var returnToDisclaimer: ImageButton
+    lateinit var goToCustom: ImageButton
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -20,23 +28,45 @@ class MenuFragment : Fragment() {
         val view =  inflater.inflate(R.layout.fragment_menu, container, false)
 
         // navigating to tips page
-        val tipsButton = view.findViewById<Button>(R.id.tipsButton)
+        tipsButton = view.findViewById<Button>(R.id.tipsButton)
         tipsButton.setOnClickListener{
             Navigation.findNavController(view).navigate(R.id.action_menuFragment_to_tipsFragment)}
 
         // navigating to disclaimer page
-        val returnToDisclaimer = view.findViewById<ImageButton>(R.id.returnToDisclaimer)
+        returnToDisclaimer = view.findViewById<ImageButton>(R.id.returnToDisclaimer)
         returnToDisclaimer.setOnClickListener{
             Navigation.findNavController(view).navigate(R.id.action_menuFragment_to_disclaimerFragment)}
 
         // navigating to customize page
-        val goToCustom = view.findViewById<ImageButton>(R.id.goToCustom)
+        goToCustom = view.findViewById<ImageButton>(R.id.goToCustom)
         goToCustom.setOnClickListener {
+            scaler()
             Navigation.findNavController(view)
                 .navigate(R.id.action_menuFragment_to_customizeFragment)
         }
 
         return view
+    }
+
+    private fun ObjectAnimator.disableViewDuringAnimation(view: View) {
+        addListener(object: AnimatorListenerAdapter(){
+            override fun onAnimationStart(animation: Animator?) {
+                view.isEnabled = false
+            }
+            override fun onAnimationEnd(animation: Animator?) {
+                view.isEnabled = true
+            }
+        })
+    }
+
+    private fun scaler() {
+        val scaleX = PropertyValuesHolder.ofFloat(View.SCALE_X,2f)
+        val scaleY = PropertyValuesHolder.ofFloat(View.SCALE_Y,2f)
+        val animator = ObjectAnimator.ofPropertyValuesHolder(goToCustom,scaleX,scaleY)
+        animator.repeatCount = 1
+        animator.repeatMode = ObjectAnimator.REVERSE
+        animator.disableViewDuringAnimation(goToCustom)
+        animator.start()
     }
 
 }
