6ba73971573b433c8c0bc28b4139ab6059e464ef
aaiovinoo <abigail.iovino@gmail.com>
2022-05-02 19:29:23 -0500

    added images to the customize page and fixed font sizes and font types for more uniformay

--- app/src/main/java/com/dotexe/homework3/InfoFragment.kt
+++ app/src/main/java/com/dotexe/homework3/InfoFragment.kt
@@ -1,5 +1,4 @@
 package com.dotexe.homework3
-
 import android.animation.Animator
 import android.animation.AnimatorListenerAdapter
 import android.animation.ObjectAnimator
@@ -8,6 +7,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageButton
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
@@ -23,10 +23,9 @@ class InfoFragment : Fragment() {
 
         backButton = view.findViewById<ImageButton>(R.id.backButton)
         backButton.setOnClickListener{
-            rotater()
+            scale(backButton)
             Navigation.findNavController(view).navigate(R.id.action_infoFragment_to_menuFragment)
         }
-
         return view
 
     }
@@ -42,26 +41,21 @@ class InfoFragment : Fragment() {
         })
     }
 
-    private fun scaler() {
-        val scaleX = PropertyValuesHolder.ofFloat(View.SCALE_X,2f)
-        val scaleY = PropertyValuesHolder.ofFloat(View.SCALE_Y,2f)
-        val animator = ObjectAnimator.ofPropertyValuesHolder(backButton,scaleX,scaleY)
-        animator.repeatCount = 1
-        animator.repeatMode = ObjectAnimator.REVERSE
-        animator.disableViewDuringAnimation(backButton)
-        animator.start()
-    }
 
-    private fun rotater() {
+
+    private fun scale(button: ImageButton) {
         // ObjectAnimator lets you do different kinds of animation
         // you could replace rotateButton with something else to animate it
         // doing a full circle rotation
         // you want 0 as the ending value
-        val animator = ObjectAnimator.ofFloat(backButton, View.ROTATION, -360f,0f)
-        //default duration is 360millisecond
-        animator.duration = 1000;
-        animator.disableViewDuringAnimation(backButton)
+        val scaleX = PropertyValuesHolder.ofFloat(View.SCALE_X,2f)
+        val scaleY = PropertyValuesHolder.ofFloat(View.SCALE_Y,2f)
+        val animator = ObjectAnimator.ofPropertyValuesHolder(button,scaleX,scaleY)
+        animator.duration = 100;
+        animator.disableViewDuringAnimation(button)
         animator.start()
     }
+
+
 
 }
